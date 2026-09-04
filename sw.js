c36315b3661c399ce2a105db58de9d97263b21f7
Peiko Family <peiko.oleh@gmail.com>
2026-09-04 22:47:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@
  * Проект: MOYAMOVA
  * Файл: sw.js
  * Назначение: Service Worker (PWA, офлайн, обновления)
- * Версия SW: 1.12.48
+ * Версия SW: 1.12.49
  * Обновлено: 2026-01-08
  * ========================================================== */
 
 'use strict';
 
 // Текущая версия SW / кэша
-const SW_VERSION = '1.12.48';
-const CACHE_NAME = 'moyamova-cache-v1.12.48';
+const SW_VERSION = '1.12.49';
+const CACHE_NAME = 'moyamova-cache-v1.12.49';
 
 // Преобразуем относительные пути в абсолютные URL на основе scope SW
 const toUrl = (path) => new URL(path, self.registration.scope).toString();
